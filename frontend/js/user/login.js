ed2f51afa068069d33fa88bd5ce88d73069ddd16
SusanaMS <smanzanaressanchez@gmail.com>
2022-04-24 16:00:01 +0200

    control del login

--- frontend/js/user/login.js
+++ frontend/js/user/login.js
@@ -4,12 +4,16 @@ import { BASE_ENDPOINT } from "../app/constants.js";
 const apiHeaders = new Headers();
 apiHeaders.append("Content-Type", "application/json");
 
-document.getElementById("loginForm").addEventListener("submit", processLogin);
+const loginForm = document.getElementById("loginForm");
+loginForm.addEventListener("submit", processLogin);
 
 function processLogin(event) {
   const emailLogin = document.getElementById("emailLogin").value;
   const paswordLogin = document.getElementById("passwordLogin").value;
+  const loginErrorMessage = document.getElementById("loginErrorMessage");
   console.debug(`enviado formulario login: ${emailLogin} / ${paswordLogin}`);
+
+  loginErrorMessage.classList.add("hidden");
 
   const jsonString = JSON.stringify({
     email: emailLogin,
@@ -30,9 +34,13 @@ function processLogin(event) {
       console.log(jsonResult);
       if (jsonResult.token != null) {
         console.log("Login correcto");
+        //almecanemos el token en la sesión web
         localStorage.setItem("jwtToken", jsonResult.token);
+        loginForm.classList.toggle("hidden");
       } else {
         console.error("Login incorrecto");
+        loginErrorMessage.innerText = jsonResult.error;
+        loginErrorMessage.classList.add("nothidden");
       }
     })
     .catch((error) => console.error("error en fetch", error));
