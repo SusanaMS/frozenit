9f62aaf79aab1423a08148441785304f7f7d3937
SusanaMS <smanzanaressanchez@gmail.com>
2022-04-24 18:06:32 +0200

    refactor

--- frontend/js/app/constants.js
+++ frontend/js/app/constants.js
@@ -1,1 +1,2 @@
 export const BASE_ENDPOINT = "http://localhost:3000/api/v1";
+export const API_CONTENT_TYPE = "application/json ";
--- frontend/js/user/login.js
+++ frontend/js/user/login.js
@@ -1,28 +1,36 @@
 // doc
-import { BASE_ENDPOINT } from "../app/constants.js";
+import { BASE_ENDPOINT, API_CONTENT_TYPE } from "../app/constants.js";
 
 const jwtToken = localStorage.getItem("jwtToken");
+const sessionUserInfo = localStorage.getItem("sessionUserInfo");
 
 const apiHeaders = new Headers();
-apiHeaders.append("Content-Type", "application/json");
+apiHeaders.append("Content-Type", API_CONTENT_TYPE);
 
 const loginForm = document.getElementById("loginForm");
 const userStatus = document.getElementById("userStatus");
+const userInfo = document.getElementById("userInfo");
 const logoutButton = document.getElementById("logoutButton");
-
-if (jwtToken != null) {
-  loginForm.classList.add("hidden");
-} else {
-  userStatus.classList.add("hidden");
-}
 
 loginForm.addEventListener("submit", processLogin);
 logoutButton.addEventListener("click", logout);
+
+if (jwtToken != null) {
+  // cuando tenemos almacenad el jwt existía un login previo
+  loginForm.classList.add("hidden");
+  userStatus.classList.add("nothidden");
+  userInfo.innerText = JSON.parse(sessionUserInfo).username;
+} else {
+  // si no es así mostramos el formulario de login
+  userStatus.classList.add("hidden");
+  loginForm.classList.add("nothidden");
+}
 
 function processLogin(event) {
   const emailLogin = document.getElementById("emailLogin").value;
   const paswordLogin = document.getElementById("passwordLogin").value;
   const loginErrorMessage = document.getElementById("loginErrorMessage");
+
   console.debug(`enviado formulario login: ${emailLogin} / ${paswordLogin}`);
 
   loginErrorMessage.classList.add("hidden");
@@ -46,12 +54,10 @@ function processLogin(event) {
       console.log(jsonResult);
       if (jsonResult.token != null) {
         console.log("Login correcto");
-        //almecanemos el token en la sesión web
+        //almecanemos el token jwt y la info de usuario
         localStorage.setItem("jwtToken", jsonResult.token);
-        localStorage.setItem("userInfo", jsonResult);
-        loginForm.classList.toggle("hidden");
-        userStatus.classList.toggle("hidden");
-        userInfo.innerText = jsonResult.username;
+        localStorage.setItem("sessionUserInfo", JSON.stringify(jsonResult));
+        location.reload();
       } else {
         console.error("Login incorrecto");
         loginErrorMessage.innerText = jsonResult.error;
