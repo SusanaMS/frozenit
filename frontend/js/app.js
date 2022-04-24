31a8e4287560da1d30ea8d4af24511220f589da0
SusanaMS <smanzanaressanchez@gmail.com>
2022-04-24 09:29:56 +0200

    primera llamada a la API

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,4 +1,6 @@
 // doc
+const apiHeaders = new Headers();
+apiHeaders.append("Content-Type", "application/json");
 
 document.getElementById("loginForm").addEventListener("submit", processLogin);
 
@@ -6,6 +8,23 @@ function processLogin(event) {
   const emailLogin = document.getElementById("emailLogin").value;
   const paswordLogin = document.getElementById("passwordLogin").value;
   console.debug(`enviado formulario login: ${emailLogin} / ${paswordLogin}`);
+
+  const raw = JSON.stringify({
+    email: emailLogin,
+    password: paswordLogin,
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: apiHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch("http://localhost:3000/api/v1/users/login/", requestOptions)
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error("error", error));
 
   event.preventDefault();
 }
