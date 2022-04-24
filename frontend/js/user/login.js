87ba8243ece47778600a504d59222e464d910d56
SusanaMS <smanzanaressanchez@gmail.com>
2022-04-24 09:57:42 +0200

    refactor

--- frontend/js/user/login.js
+++ frontend/js/user/login.js
@@ -1,4 +1,6 @@
 // doc
+import { BASE_ENDPOINT } from "../app/constants.js";
+
 const apiHeaders = new Headers();
 apiHeaders.append("Content-Type", "application/json");
 
@@ -9,7 +11,7 @@ function processLogin(event) {
   const paswordLogin = document.getElementById("passwordLogin").value;
   console.debug(`enviado formulario login: ${emailLogin} / ${paswordLogin}`);
 
-  const raw = JSON.stringify({
+  const jsonString = JSON.stringify({
     email: emailLogin,
     password: paswordLogin,
   });
@@ -17,11 +19,12 @@ function processLogin(event) {
   const requestOptions = {
     method: "POST",
     headers: apiHeaders,
-    body: raw,
+    body: jsonString,
     redirect: "follow",
   };
 
-  fetch("http://localhost:3000/api/v1/users/login/", requestOptions)
+  console.log(`${BASE_ENDPOINT}/users/login/`);
+  fetch(`${BASE_ENDPOINT}/users/login/`, requestOptions)
     .then((response) => response.text())
     .then((result) => console.log(result))
     .catch((error) => console.error("error", error));
