e3a56a4ffccea772232ede4b8e0bfbeec58ba39a
SusanaMS <smanzanaressanchez@gmail.com>
2022-05-01 16:51:30 +0200

    selects para congelador y alimento

--- frontend/js/api/records.js
+++ frontend/js/api/records.js
@@ -4,6 +4,7 @@ import {
   jsonArray2htmlTable,
   array2option,
   checkJWT,
+  getUserEmail,
 } from "../common/utils.js";
 
 const MODEL_ENPOINT = "records";
@@ -11,10 +12,17 @@ const jwtToken = localStorage.getItem("jwtToken");
 const recordGet = document.getElementById("recordGet");
 const recordBox = document.getElementById("recordBox");
 const recordBoxMessage = document.getElementById("recordBoxMessage");
+const recordAdd = document.getElementById("recordAdd");
+const recordBoxAdd = document.getElementById("recordBoxAdd");
+const recordFreezer = document.getElementById("recordFreezer");
+const recordFood = document.getElementById("recordFood");
+const recordAddErrorMessage = document.getElementById("recordAddErrorMessage");
+const addRecordForm = document.getElementById("addRecordForm");
 
 let apiHeaders;
 
 recordGet.addEventListener("click", processRecordGet);
+recordAdd.addEventListener("click", clickRecordAdd);
 
 function processRecordGet(event) {
   console.log("entroo!!");
@@ -22,11 +30,8 @@ function processRecordGet(event) {
     return null;
   }
 
-  const email = JSON.parse(localStorage.getItem("sessionUserInfo")).email;
-
+  const email = getUserEmail();
   if (email == null) {
-    console.error("No se puede obtener el mail de usuario");
-    window.alert("Error al obtener el id de usuario");
     return null;
   }
 
@@ -81,6 +86,87 @@ function processRecordGet(event) {
     .catch((error) =>
       apiError(true, recordBoxMessage, endpoint, error.message)
     );
+
+  event.preventDefault();
+}
+
+function recordFreezerSelect(email) {
+  apiHeaders = new Headers();
+  apiHeaders.append("Content-Type", API_CONTENT_TYPE);
+  apiHeaders.append("Authorization", `Bearer ${jwtToken}`);
+
+  const requestOptions = {
+    method: "GET",
+    headers: apiHeaders,
+    redirect: "follow",
+  };
+
+  // necesitamos para las select del formulario tanto el congelador
+  // como el alimento, ya que son PK de la tabla destino junto con la id de user
+
+  const endpoint = `${BASE_ENDPOINT}/freezers/email/${email}`;
+
+  console.log(endpoint, requestOptions);
+  fetch(endpoint, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const jsonResult = JSON.parse(result);
+      if (jsonResult.error != null) {
+        apiError(false, recordAddErrorMessage, endpoint, jsonResult.error);
+      } else {
+        console.log(jsonResult);
+        const userFreezers = jsonResult.map(({ name_freezer }) => name_freezer);
+        array2option(userFreezers, recordFreezer);
+      }
+    })
+    .catch((error) => {
+      apiError(true, recordAddErrorMessage, endpoint, error.message);
+    });
+}
+
+function recordFoodSelect() {
+  apiHeaders = new Headers();
+  apiHeaders.append("Content-Type", API_CONTENT_TYPE);
+  apiHeaders.append("Authorization", `Bearer ${jwtToken}`);
+
+  const requestOptions = {
+    method: "GET",
+    headers: apiHeaders,
+    redirect: "follow",
+  };
+
+  const endpoint = `${BASE_ENDPOINT}/foods/all`;
+
+  fetch(endpoint, requestOptions)
+    .then((response) => response.text())
+    .then((result) => {
+      const jsonResult = JSON.parse(result);
+      if (jsonResult.error != null) {
+        apiError(false, recordAddErrorMessage, endpoint, jsonResult.error);
+      } else {
+        console.log(jsonResult);
+        const foods = jsonResult.map(({ name_food }) => name_food);
+        array2option(foods, recordFood);
+      }
+    })
+    .catch((error) => {
+      apiError(true, recordAddErrorMessage, endpoint, error.message);
+    });
+}
+
+function clickRecordAdd(event) {
+  if (!checkJWT(jwtToken)) {
+    return null;
+  }
+
+  const email = getUserEmail();
+  if (email == null) {
+    return null;
+  }
+
+  Promise.all([recordFreezerSelect(email), recordFoodSelect()]).then(
+    recordBoxAdd.setAttribute("style", "display: flex")
+  );
 
   event.preventDefault();
 }
