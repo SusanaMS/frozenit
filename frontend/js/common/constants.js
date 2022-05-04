8fb05f3386fb61eb2677985880f0b27826f05894
SusanaMS <smanzanaressanchez@gmail.com>
2022-05-04 20:10:58 +0200

    refactor

--- frontend/js/common/constants.js
+++ frontend/js/common/constants.js
@@ -1,4 +1,4 @@
 export const BASE_ENDPOINT = "http://localhost:3000/api/v1";
 export const API_CONTENT_TYPE = "application/json ";
 export const BOX_CLASSES = "info-box";
-export const REF_CLASSES = "accionRef";
+export const REF_CLASSES = "accion-ref";
